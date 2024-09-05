793935ff67e51e1359ebd8b5a1937a82f58d60f6
”sweet-and-sour24” <sweet.and.sour24@icloud.com>
2024-09-05 13:15:59 +0900

    課題提出

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="jp">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>PHP基礎課題</title>
+</head>
+<body>
+  <p>
+    <?php
+    $merchandise_data = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+    print_r($merchandise_data);
+    ?>
+  </p>
+</body>
+</html>
